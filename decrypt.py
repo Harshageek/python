3f11eff2c9d334019a47c10136ae36320a329198
Harshageek <92079592+Harshageek@users.noreply.github.com>
2023-11-06 09:42:41 +0530

    encrypt/decrypt

---
+++ decrypt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+from cryptography.fernet import Fernet
+
+files=[]
+user = input("file name to decrypt : ")
+for file in os.listdir():
+	if file == user:
+		pass
+	else :
+		continue
+	files.append(file)
+with open("thekey.key","rb") as key:
+	s_key = key.read()
+
+for file in files:
+	with open(file,"rb") as thefile:
+		co = thefile.read()
+	en=Fernet(s_key).decrypt(co)
+	with open(file,"wb") as thefile:
+		thefile.write(en)
+
+print(f"{file}\nThe file have be decrypted successfully")
